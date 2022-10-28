d3195dd9c96e1844fe4c3c97cfc0f31c92bec027
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-10-28 20:08:09 +0100

    feat(backend): add `/api/users` route

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/posts', require('./routes/postRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
