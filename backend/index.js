6a58003caf183f20de75acaa2949d40882700d51
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-11-16 22:13:35 +0000

    feat(backend): add `cors`

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const connectDB = require('./config/db.js');
@@ -10,6 +11,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
