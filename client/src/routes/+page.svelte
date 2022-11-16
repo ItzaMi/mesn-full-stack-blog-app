8838cd7463f8630d7de0b54abb7d1b2d37e9950f
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-11-16 22:14:01 +0000

    feat(client): SvelteKit setup + initial fetch to API to see if it's working

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let posts: any[] = [];
+
+	onMount(async () => {
+		await fetch(`http://localhost:4000/api/posts`)
+			.then((r) => r.json())
+			.then((data) => {
+				posts = data;
+			});
+	});
+</script>
+
+{#if posts && posts.length > 0}
+	{#each posts as post}
+		<div>
+			<h1>{post.title}</h1>
+		</div>
+	{/each}
+{:else}
+	<p>There are no posts</p>
+{/if}
